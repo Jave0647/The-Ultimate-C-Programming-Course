868d925fa06eb1b2bec3934d2b6a4a8fe84cb5a9
Jave0647 <javedmohd0647@gmail.com>
2024-12-05 01:09:18 +0530

    Update 04_problem4.c

--- Chapter 8 - Practice Set/04_problem4.c
+++ Chapter 8 - Practice Set/04_problem4.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 
-char* slice(char str[], int m, int n){
-    int i=0, count;  
+char* slice(char str[], int m, int n){  
     char *ptr1 = &str[m];
-    char *ptr2 = &str[n];
-
     str = ptr1;
     str[n] = '\0';
     return str;
